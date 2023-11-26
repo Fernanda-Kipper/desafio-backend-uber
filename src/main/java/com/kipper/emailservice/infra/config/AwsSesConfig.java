9c52cba1ac900a26cf1d39b801eff6c5f8453acf
Fernanda <nanda.kipper@gmail.com>
2023-11-26 15:05:00 -0300

    feat: configuring aws

--- src/main/java/com/kipper/emailservice/infra/config/AwsSesConfig.java
+++ src/main/java/com/kipper/emailservice/infra/config/AwsSesConfig.java
@@ -2,15 +2,18 @@ package com.kipper.emailservice.infra.config;
 
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AwsSesConfig {
+    @Value("${aws.region}")
+    private String awsRegion;
 
     @Bean
     public AmazonSimpleEmailService amazonSimpleEmailService() {
-        return AmazonSimpleEmailServiceClientBuilder.standard().build();
+        return AmazonSimpleEmailServiceClientBuilder.standard().withRegion(this.awsRegion).build();
     }
 }
 
