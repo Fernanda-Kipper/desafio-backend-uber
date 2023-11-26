42c2b8fce43b517e172f3af2a80bf85bd7cf096e
Fernanda <nanda.kipper@gmail.com>
2023-11-26 15:41:12 -0300

    fix: adjust aws credentials config

--- src/main/java/com/kipper/emailservice/infra/config/AwsSesConfig.java
+++ src/main/java/com/kipper/emailservice/infra/config/AwsSesConfig.java
@@ -1,5 +1,7 @@
 package com.kipper.emailservice.infra.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,9 +13,21 @@ public class AwsSesConfig {
     @Value("${aws.region}")
     private String awsRegion;
 
+    @Value("${aws.accessKeyId}")
+    private String accessKey;
+
+    @Value("${aws.secretKey}")
+    private String secretKey;
+
     @Bean
     public AmazonSimpleEmailService amazonSimpleEmailService() {
-        return AmazonSimpleEmailServiceClientBuilder.standard().withRegion(this.awsRegion).build();
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        return AmazonSimpleEmailServiceClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(awsRegion)
+                .build();
     }
 }
 
